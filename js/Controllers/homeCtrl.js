9bc47587cf79d2921f49ed7da527a35239b5b12a
CodeMaster7 <italianstalian_2@yahoo.com>
2016-02-18 03:00:31 -0700

    added js to the navbar

--- js/Controllers/homeCtrl.js
+++ js/Controllers/homeCtrl.js
@@ -1,9 +1,19 @@
 var app = angular.module('bookKart');
 
-app.controller('homeCtrl', function ($scope){
+app.controller('homeCtrl', function ($scope, $location){
 	
 	$scope.appTitle = {
 		title: "Book Kart",
 		tagLine: "We have a collection of 1 Million books!"
 	};
+	
+	
+	$scope.nav = {};
+	$scope.nav.isActive = function (path) {
+		if (path === $location.path()) {
+			return true;
+		}
+		
+		return false;
+	}
 });
