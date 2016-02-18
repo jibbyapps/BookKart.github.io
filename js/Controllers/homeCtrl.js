bbcb231bc1035f24ab5dfe88669389df5bd39197
CodeMaster7 <italianstalian_2@yahoo.com>
2016-02-17 22:42:06 -0700

    added a title and a tagline key into the object

--- js/Controllers/homeCtrl.js
+++ js/Controllers/homeCtrl.js
@@ -1,3 +1,9 @@
 var app = angular.module('bookKart');
+
 app.controller('homeCtrl', function ($scope){
+	
+	$scope.appTitle = {
+		title: "Book Kart",
+		tagLine: "We have a collection of 1 Million books!"
+	};
 });
