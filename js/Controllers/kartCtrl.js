567bf9fd2a6bd5996dca51f65d5eae66e055b249
CodeMaster7 <italianstalian_2@yahoo.com>
2016-02-18 02:00:18 -0700

    added javascript

--- js/Controllers/kartCtrl.js
+++ js/Controllers/kartCtrl.js
@@ -1,4 +1,12 @@
 var app = angular.module('bookKart');
 
-app.controller('kartCtrl', function ($scope){
+app.controller('kartCtrl', function ($scope, kartService){
+	
+	$scope.kart = kartService.getKart();
+	
+	$scope.buy = function (book) {
+		kartService.buy(book);
+		console.log("buy: ", book);
+	}
+	
 });
